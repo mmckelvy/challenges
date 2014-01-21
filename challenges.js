ceb54e205a5d91db8994d5c4c2aa6946ef13d9da
Mark McKelvy <mmckelvy@gmail.com>
2014-01-20 21:32:52 -0700

    first commit

---
+++ challenges.js
@@ -0,0 +1,20 @@
+// Calculate the factorial of a number.
+var factorial = function (number) {
+	var factorsArr = [];
+	// Get all the factors.
+	for (var i = number; i > 1; i--) {
+		factorsArr.push(i);
+	}
+	// Multiply the factors
+	var value = factorsArr.reduce(function (a, b) {
+		return a * b;
+	});
+
+	return value;
+};
+
+console.log(factorial(4));
+console.log(factorial(8));
+
+// Sort function.
+
