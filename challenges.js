a52796fc92c64b17c4c99a4dfd64907ac62833aa
Mark McKelvy <mmckelvy@gmail.com>
2014-01-21 22:45:18 -0700

    re-ordered coded

--- challenges.js
+++ challenges.js
@@ -15,3 +15,32 @@ var factorial = function (number) {
 
 console.log(factorial(4));
 console.log(factorial(8));
+
+// Sort characters of a string alphabetically.
+var alphaSort = function (str) {
+	var outputArr = [];
+	// Create a compare function for the subsequent sort.
+	var compareFunc = function (a, b) {
+		if (a < b) {
+			return -1;
+		}
+		else if (b < a) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	};
+	// Push characters to output array.
+	for (var i = 0; i < str.length; i++) {
+		outputArr.push(str.charAt(i));
+	}
+	// Sort the array.
+	outputArr.sort(compareFunc);
+	var sorted = outputArr.join("");
+
+	return sorted;
+};
+
+var testString = "stuf";
+console.log(alphaSort(testString));
